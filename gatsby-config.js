04aea881f3c89f592097a0d5ebade9277efedb5c
hammadsheikh <hammadsheikh79@gmail.com>
2021-09-20 10:50:13 -0400

    Meta Info updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,75 +1,79 @@
 module.exports = {
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://hammadsheikh.github.io/`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Hammad Sheikh',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Hammad Sheikh | Quantitative Researcher`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: `Qantitative psychologist, looking for maximing impact`,
     // Optional: Twitter account handle
     author: `@rfitzio`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/hammadsheikh`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/hammad-sheikh/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `1. Ph.D. in Cognitive, Social, and Developmental Psychology; designed experimental studies to test theories on group-based cognition and decisions making in adults and children
+    2. Broad skillset and proven effective collaborations in a wide range of disciplines: cognitive science, statistical modeling, data science, machine learning, computational social science, data visualization, data sonification, experimental design, multi-method studies
+    3. Deep expertise in quantifying human cognition (psychometrics), designing experimental tests of hypotheses, and translating research findings into actionable insights
+    4. Lectured many university-level courses such as Intro to Statistics and Research Design, History & Politics of Terrorism, R for psychologists; presented to professional and non-professional audiences; translated qualitative and quantitative research findings and into actionable insights for decision makers (media companies & governments)
+    5. Extensive teamwork and collaboration with peers across disciplines and around the globe; trained field worker, mentored junior researchers
+    6. Self-starter, quick study, individual contributor, problem solver, teacher & mentor, always learning`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
-        name: 'Devfolio',
+        name: 'Visualizing Terrorism',
         description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'Dashboard: Customizable Visualization (& Animation) of Terrorism Attacks Around the World',
+        link: 'https://github.com/hammadsheikh/visualizingterorrism',
       },
       {
-        name: 'ChromeExtensionKit',
+        name: 'Query: Global Terrorism Database',
         description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://chromeextensionkit.com/?ref=devfolio',
-      },
-      {
-        name: 'Another Cool Project',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+          'SQL Query for the Global Terrorism Database',
+        link: 'https://github.com/hammadsheikh/SQLGTD',
       },
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Big Bad Boo Studios',
+        description: 'Lead Quantitative Researcher, May 2017 - Present',
+        link: 'https://www.bigbadboo.com/bigbadboo/about.html',
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Centre for the Resolution of Intracatable Conflicts (Oxford University)',
+        description: 'Visiting Researcher, December 2015 - Present',
+        link: 'http://cric-oxford.org/',
       },
       {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'New School for Social Research',
+        description: 'Postdoctoral Research Fellow, September 2018 - August 2021',
+        link: 'https://www.newschool.edu/nssr/faculty/Hammad-Sheikh/',
+      },
+      {
+        name: 'Artis International',
+        description: 'Young Investigator, September 2018 - August 2021',
+        link: 'https://artisinternational.org/category/hammad-sheikh/',
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
-        name: 'Languages & Frameworks',
+        name: 'Languages & Tools',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'R (tidyverse, lme4, caret, gpart), Python (pandas, numpy), SQL, SPSS, Amos, SAS, Mplus, AWS',
       },
       {
-        name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+        name: 'Statistics & ML',
+        description: 'Supervised and Unsuyprevised (incl. Anova, Linear & Logistic Regression, Multi-Level Models, PCA, FA, CFA, Cluster Analysis',
       },
       {
-        name: 'Other',
+        name: 'Project Management',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'Trello, Slack, MS Teams',
       },
     ],
   },
